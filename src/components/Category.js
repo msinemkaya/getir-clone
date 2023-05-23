b16be756ce17096267b4992b58a9417fb639f099
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-23 22:03:08 +0300

    fix: styling fix

--- src/components/Category.js
+++ src/components/Category.js
@@ -3,9 +3,9 @@ export default function Category({ category }){
   const {title, image, id} = category
 
   return(
-    <a href='#' className='flex flex-col items-center text-center p-4 gap-y-2 transition hover:bg-purple-50 rounded-md min-h-[8rem] group'>
+    <a href='#' className='flex flex-col items-center text-center p-4 gap-y-2 transition hover:bg-purple-50 rounded-md group'>
       <img src={image} alt='' className='w-12 h-12 rounded border border-gray-200' />
-      <span className='text-sm font-semibold leading-tight text-gray-700 group-hover:text-brand-color'>{title}</span>
+      <span className='text-sm font-semibold leading-tight text-gray-700 whitespace-nowrap group-hover:text-brand-color'>{title}</span>
     </a>
   );
 }
